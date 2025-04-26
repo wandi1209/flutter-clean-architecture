b7731b18dc906414443a71c07bbe822dc88e31a6
wandi1209 <wandider@gmail.com>
2025-04-26 08:49:42 +0700

    feat(profile): add ProfileModel with fromJson and toJson

---
+++ lib/features/profile/data/models/profile_model.dart
@@ -0,0 +1,36 @@
+import 'package:clean_architecture/features/profile/domain/entities/profile.dart';
+
+class ProfileModel extends Profile {
+  final String firstName;
+  final String lastName;
+  final String avatar;
+
+  const ProfileModel({
+    required super.id,
+    required super.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  }) : super(fullName: "$firstName $lastName", profileUrl: avatar);
+
+  factory ProfileModel.fromJson(Map<String, dynamic> dataJson) {
+    Map<String, dynamic> data = dataJson["data"];
+    return ProfileModel(
+      id: data["id"],
+      email: data["email"],
+      firstName: data["first_name"],
+      lastName: data["last_name"],
+      avatar: data["avatar"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "email": email,
+      "first_name": firstName,
+      "last_name": lastName,
+      "avatar": avatar,
+    };
+  }
+}
